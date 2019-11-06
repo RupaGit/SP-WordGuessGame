dcbe62900fcacf2015169326648158daa34d9083
spopuri <spopuri@mediaocean.com>
2019-11-06 11:49:05 -0500

    Pushing changes that are still in progress

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -4,6 +4,8 @@
     var noOfGuesses = 6;
     var divGetter = document.getElementById("letterArea");
     var wordLetters = wordChosen.split("");
+    var letter, button, holder;
+    var userChoices = [];
     
     // function to generate empty boxes based on length of the word
     function hangManChooseRandomWord(event) {
@@ -13,28 +15,26 @@
             var cell = row.insertCell(-1);     
         }
     }
-
-    
-
-       window.addEventListener( "load", function( windowLoadE ) {
-            var p, letter, button, holder;
-            holder = document.getElementById( "buttonsHolder" );
-            for ( var i = 65; i <= 90; i++ ) {
-                if ( i == 65 || i == 75 || i == 84 ) {
-                    p = document.createElement( "p" );
-                }
-                letter = String.fromCharCode( i );
-                button = document.createElement( "button" );
-                button.innerHTML = letter;
-                button.setAttribute( "data-letter", letter );
-                button.onclick = function( e ) { setLetter( this.getAttribute( "data-letter" ) ); };
-                p.appendChild( button );
-                if ( i == 74 || i == 83 || i == 90 ) {
-                    holder.appendChild( p );
-                }
-            }
-        } );
-        function setLetter( letter ) {
-            var div = document.getElementById( "name" );
-            div.innerHTML = div.innerHTML + letter;
-        } 
+   
+    window.addEventListener( "load", function( windowLoadE ) {
+    var p, letter, button, holder;
+    holder = document.getElementById( "buttonsHolder" );
+    for ( var i = 65; i <= 90; i++ ) {
+        if ( i == 65 ) {
+            p = document.createElement( "p" );
+        }
+        letter = String.fromCharCode( i );
+        button = document.createElement( "button" );
+        button.innerHTML = letter;
+        button.setAttribute( "data-letter", letter );
+        button.onclick = function( e ) { setLetter( this.getAttribute( "data-letter" ) ); };
+        p.appendChild( button );
+        if ( i == 74 || i == 83 || i == 90 ) {
+            holder.appendChild( p );
+        }
+    }
+    } );
+    function setLetter( letter ) {
+        userChoices.push(letter);
+        //console.log(userChoices);
+    } 
